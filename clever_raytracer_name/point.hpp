829c9e353d72335227185bd2194c004d90694c7e
heinmnn2 <heinmnn2@illinois.edu>
2018-02-18 19:09:59 -0600

    point.hpp

--- clever_raytracer_name/point.hpp
+++ clever_raytracer_name/point.hpp
@@ -1,7 +1,6 @@
 #ifndef __POINT__
 #define __POINT__
 
-<<<<<<< HEAD
 //#include "ray.hpp"
 
 namespace raytrace
@@ -9,15 +8,6 @@ namespace raytrace
     template<typename T> class Vector;
     typedef Vector<float> Vect;
 }
-=======
-namespace raytrace {
-  template<typename T> class Vector;
-  typedef Vector<float> Vect;
-}
-
-// We do cross-includes, so we will forward declare a handful of things.
-
->>>>>>> 397731fc3154517cb058a8f4b5e83c32c7e1a7b3
 
 class Point
 {
@@ -45,38 +35,36 @@ public:
     double distance(const Point& p) const;
 };
 
-<<<<<<< HEAD
 /*inline Point Point::operator- (void) const
-{
-    return (Point(-x, -y, -z));
-}
+ {
+ return (Point(-x, -y, -z));
+ }
+ 
+ inline Point Point::operator- (const Point& p) const
+ {
+ return (raytrace::Vect(x - p.x, y - p.y, z - p.z));
+ }
+ 
+ inline Point Point::operator- (const raytrace::Vect& v) const
+ {
+ return (Point(x - v.x, y - v.y, z - v.z));
+ }
+ 
+ inline Point Point::operator* (const double a) const
+ {
+ return (Point(x * a, y * a, z * a));
+ }
+ 
+ inline double Point::d_squared(const Point& p) const
+ {
+ return ((x - p.x) * (x - p.x) + (y - p.y) * (y - p.y) + (z - p.z) * (z - p.z));
+ }
+ 
+ inline Point operator* (double a, const Point& p)
+ {
+ return (Point(a * p.x, a * p.y, a * p.z));
+ }
+ */
 
-inline Point Point::operator- (const Point& p) const
-{
-    return (raytrace::Vect(x - p.x, y - p.y, z - p.z));
-}
-
-inline Point Point::operator- (const raytrace::Vect& v) const
-{
-    return (Point(x - v.x, y - v.y, z - v.z));
-}
-
-inline Point Point::operator* (const double a) const
-{
-    return (Point(x * a, y * a, z * a));
-}
-
-inline double Point::d_squared(const Point& p) const
-{
-    return ((x - p.x) * (x - p.x) + (y - p.y) * (y - p.y) + (z - p.z) * (z - p.z));
-}
-
-inline Point operator* (double a, const Point& p)
-{
-    return (Point(a * p.x, a * p.y, a * p.z));
-}
-*/
-
-=======
->>>>>>> 397731fc3154517cb058a8f4b5e83c32c7e1a7b3
 #endif
+
