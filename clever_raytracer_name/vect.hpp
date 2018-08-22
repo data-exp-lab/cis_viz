70111ccbc52f418ca1c46e0f0f750b42fcbfbf29
heinmnn2 <heinmnn2@illinois.edu>
2018-08-22 15:52:03 -0500

    update vect class

--- clever_raytracer_name/vect.hpp
+++ clever_raytracer_name/vect.hpp
@@ -6,78 +6,80 @@ class Vect
     double x;
     double y;
     double z;
-
+    
 public:
-    Vect();
-    Vect(double, double, double);
-
+    
+    Vect ();
+    
+    Vect (double, double, double);
+    
+    // method functions
     double getVectX()
     {
         return x;
     }
-
+    
     double getVectY()
     {
         return y;
     }
-
+    
     double getVectZ()
     {
         return z;
+        
     }
-
-    double magnitude ()
+    
+    double magnitude()
     {
-        return sqrt((x*x) + (y*y) + (z*z));
+        return sqrt((x * x) + (y * y) + (z * z));
     }
-
-    Vect normalize ()
+    
+    Vect normalize()
     {
-        double magnitude = sqrt((x*x) + (y*y) + (z*z));
-        return Vect (x/magnitude, y/magnitude, z/magnitude);
+        double magnitude = sqrt((x * x) + (y * y) + (z * z));
+        return (Vect(x / magnitude, y / magnitude, z / magnitude));
     }
-
+    
     Vect negative ()
     {
-        return Vect (-x, -y, -z);
+        return (Vect(-x, -y, -z));
     }
-
+    
     double dotProduct(Vect v)
     {
         return (x * v.getVectX() + y * v.getVectY() + z * v.getVectZ());
     }
-
+    
     Vect crossProduct(Vect v)
     {
-        return Vect(y + v.getVectZ() - z * v.getVectY(), z * v.getVectX() - x * v.getVectZ(), x * v.getVectY() - y * v.getVectX());
+        return (Vect(y * v.getVectZ() - z * v.getVectY(), z * v.getVectX() - x * v.getVectZ(), x * v.getVectY() - y * v.getVectX()));
     }
-
+    
     Vect vectAdd(Vect v)
     {
-        return Vect(x + v.getVectX(), y + v.getVectY(), z + v.getVectZ());
+        return (Vect(x + v.getVectX(), y + v.getVectY(), z + v.getVectZ()));
     }
-
-    Vect vectMult(double scalar)
+    
+    Vect vectMult (double scalar)
     {
-        return Vect(x * scalar, y * scalar, z * scalar);
+        return (Vect(x * scalar, y * scalar, z * scalar));
     }
-
 };
 
-Vect::Vect()
+Vect::Vect ()
 {
     x = 0;
     y = 0;
     z = 0;
 }
 
-Vect::Vect(double i, double j, double k)
+Vect::Vect (double i, double j, double k)
 {
     x = i;
     y = j;
     z = k;
 }
-
 
 
 #endif
